5ff5a7091110839dad748cac1dd5b2c378de1dad
Mehran Khani <mehran.khani93@gmail.com>
2024-08-19 19:18:46 +0300

    Refined login and registration screens UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -42,44 +42,83 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                TextFormField(
-                  key: const Key('emailField'),
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Welcome Back!',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          SizedBox(
+                            width: MediaQuery.sizeOf(context).width * 0.7,
+                            child: const Text(
+                              'Log in to your account and continue managing your tasks efficiently.',
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 32,
+                          ),
+                          TextFormField(
+                            key: const Key('emailField'),
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            key: const Key('passwordField'),
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
                 ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  key: const Key('passwordField'),
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 32.0),
+                const SizedBox(height: 8.0),
                 BlocConsumer<AuthenticationCubit, AuthenticationState>(
                   listener: (context, state) async {
                     if (state is Authenticated) {
@@ -89,7 +128,6 @@ class LoginScreenState extends State<LoginScreen> {
                           backgroundColor: Colors.green,
                         ),
                       );
-
                       if (state.user.isVerified) {
                         Navigator.pushReplacementNamed(context, '/home');
                       } else {
@@ -100,33 +138,47 @@ class LoginScreenState extends State<LoginScreen> {
                         );
                       }
                     } else if (state is LoggedOut && state.error != null) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(
-                            state.error ?? 'Failed to Login. Please try again'),
-                        backgroundColor: Colors.red,
-                      ));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.error ??
+                              'Failed to Login. Please try again'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
                     }
                   },
-                  builder: (context, state) => ElevatedButton(
-                    onPressed: () async => await _submitForm(context),
-                    child: state.isLoading
-                        ? const Center(child: CircularProgressIndicator())
-                        : const Text('Login'),
+                  builder: (context, state) => SizedBox(
+                    width: MediaQuery.sizeOf(context).width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      onPressed: () async => await _submitForm(context),
+                      child: state.isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : const Text('Login'),
+                    ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/register',
-                      (route) => route.isFirst,
-                    );
-                  },
-                  child: const Text('Go to Register'),
+                const SizedBox(height: 4.0),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/register',
+                        (route) => route.isFirst,
+                      );
+                    },
+                    child: const Text('Don\'t have an account? Sign Up now'),
+                  ),
                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -44,111 +44,193 @@ class RegistrationScreenState extends State<RegistrationScreen> {
       appBar: AppBar(
         title: const Text('Register'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                key: const Key('emailField'),
-                controller: _emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                key: const Key('passwordField'),
-                controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                key: const Key('confirmPasswordField'),
-                controller: _confirmPasswordController,
-                decoration:
-                    const InputDecoration(labelText: 'Confirm Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please confirm your password';
-                  }
-                  if (value != _passwordController.text) {
-                    return 'Passwords do not match';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                key: const Key('nameField'),
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              BlocConsumer<AuthenticationCubit, AuthenticationState>(
-                listener: (context, state) async {
-                  if (state is Authenticated) {
-                    // Show the success message in a SnackBar
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(state.message),
-                        backgroundColor: Colors.green,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Welcome to ToDoLo!',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: const Text(
+                              'Get ready to boost your productivity and stay on top of your tasks. Let\'s get you set up!',
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          const SizedBox(height: 32),
+                          TextFormField(
+                            key: const Key('emailField'),
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                  .hasMatch(value)) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextFormField(
+                            key: const Key('nameField'),
+                            controller: _nameController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Your Name',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your name';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextFormField(
+                            key: const Key('passwordField'),
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 8.0),
+                          TextFormField(
+                            key: const Key('confirmPasswordField'),
+                            controller: _confirmPasswordController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              hintText: 'Confirm your password',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please confirm your password';
+                              }
+                              if (value != _passwordController.text) {
+                                return 'Passwords do not match';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 32,
+                          )
+                        ],
                       ),
-                    );
+                    ),
+                  ),
+                ),
+                BlocConsumer<AuthenticationCubit, AuthenticationState>(
+                  listener: (context, state) async {
+                    if (state is Authenticated) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.message),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
 
-                    if (state.user.isVerified) {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    } else {
-                      Navigator.pushReplacementNamed(
-                        context,
-                        '/verify-email',
-                        arguments: state.user.email,
+                      if (state.user.isVerified) {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      } else {
+                        Navigator.pushReplacementNamed(
+                          context,
+                          '/verify-email',
+                          arguments: state.user.email,
+                        );
+                      }
+                    } else if (state is Registering && state.error != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.error ??
+                              'Failed to register. Please try again'),
+                          backgroundColor: Colors.red,
+                        ),
                       );
                     }
-                  } else if (state is Registering && state.error != null) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(state.error ??
-                          'Failed to register. Please try again'),
-                      backgroundColor: Colors.red,
-                    ));
-                  }
-                },
-                builder: (context, state) => ElevatedButton(
-                  onPressed: () async => await _submitForm(context),
-                  child: state.isLoading
-                      ? const Center(child: CircularProgressIndicator())
-                      : const Text('Register'),
+                  },
+                  builder: (context, state) => Column(
+                    children: [
+                      const SizedBox(height: 8.0),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                          onPressed: () async => await _submitForm(context),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          child: state.isLoading
+                              ? const Center(child: CircularProgressIndicator())
+                              : const Text('Register'),
+                        ),
+                      ),
+                      const SizedBox(height: 4.0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.popUntil(context, (route) => route.isFirst);
+                          Navigator.pushReplacementNamed(context, '/login');
+                        },
+                        child: const Text('Already have an account? Login Now'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                  Navigator.pushReplacementNamed(context, '/login');
-                },
-                child: const Text('Go to Login'),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
